2e17f850ff1dea55a6656987266981bce9e1a4f3
Ehsan-saradar <ehsan.saradar@gmail.com>
2026-02-10 13:01:30 +0330

    Add 'loaded' flag to AppProvider state

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -272,9 +272,10 @@ export const AppPage = () => {
                     ) : (
                       <Button
                         onClick={() =>
-                          startReshare(id).then((isInstalled) =>
-                            setState((prev) => ({ ...prev, isInstalled })),
-                          )
+                          startReshare(id).then((isInstalled) => {
+                            setState((prev) => ({ ...prev, isInstalled }));
+                            navigate(modalHash.success);
+                          })
                         }
                       >
                         Get
